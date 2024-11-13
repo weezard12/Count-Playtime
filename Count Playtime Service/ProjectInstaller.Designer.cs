bcfef462223116a63263242d88a9d700ae868f4c
weezard12 <weezard1234@gmail.com>
2024-11-13 23:34:39 +0200

    updated count playtime service

--- Count Playtime Service/ProjectInstaller.Designer.cs
+++ Count Playtime Service/ProjectInstaller.Designer.cs
@@ -41,6 +41,7 @@
             this.serviceInstaller1.DisplayName = "Count Playtime";
             this.serviceInstaller1.Description = "CountPlaytime";
             this.serviceInstaller1.ServiceName = "Service1";
+            this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
 
             this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
             // 
